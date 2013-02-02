793cb68088b23824be39c81928b9ef9c55685c06
kimothy <kimtengh@gmail.com>
2013-02-02 13:36:12 +0100

    added voltage function and readout in with the "staus" command.

--- fan.ino
+++ fan.ino
@@ -71,6 +71,7 @@ int checkCommand() {
 		mode = 5;
 	}
 	else {
+		Serial.println("Not a valid command");
 		mode = 0;
 	}
 	return mode;
@@ -113,6 +114,8 @@ void executeCommand(int mode) {
 				}
 			Serial.print("\n");
 			}
+		Serial.print(readVcc(), DEC );
+		Serial.println("mV");
 		}
 	delay(100);
 	mode = 0;
@@ -162,6 +165,18 @@ int push(){
 	}
 	delay(1);
 	return mode;
+}
+long readVcc() {
+	long result;
+	// Read 1.1V reference against AVcc
+	ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+	delay(2); // Wait for Vref to settle
+	ADCSRA |= _BV(ADSC); // Convert
+	while (bit_is_set(ADCSRA,ADSC));
+	result = ADCL;
+	result |= ADCH<<8;
+	result = 1126400L / result; // Back-calculate AVcc in mV
+	return result;
 }
 /* end of remove */
 void loop() {
